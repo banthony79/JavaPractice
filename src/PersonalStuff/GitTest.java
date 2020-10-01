881235581afae8df07ba89d1a82013a7aa24e8b0
Bryce Looyenga <blooyeng@gmail.com>
2020-09-30 21:00:39 -0600

    gittest added

---
+++ src/PersonalStuff/GitTest.java
@@ -0,0 +1,16 @@
+package PersonalStuff;
+
+public class GitTest {
+
+    public static String yay() {
+        return "yay!!";
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(yay());
+        }
+        System.out.println("It works!");
+    }
+}

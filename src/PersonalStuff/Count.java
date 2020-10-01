b5e9082be6ba02449f44753fdbce60f4462a6f18
Bryce Looyenga <blooyeng@gmail.com>
2020-09-30 21:22:00 -0600

    adding count.java

---
+++ src/PersonalStuff/Count.java
@@ -0,0 +1,11 @@
+package PersonalStuff;
+
+public class Count {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am the count!");
+
+        //testing Github commits
+    }
+}
